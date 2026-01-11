342a58b1dda78bc1b0ec71fa65cb3f4b06710a61
beautyfool2023 <tjkls_cn@hotmail.com>
2026-01-11 19:29:39 +0800

    fix image path and add worx product

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,8 @@ export default function Home() {
             price="$399.99"
             // ✅ REMINDER: Ensure this image is moved to your /public folder
             image="/413FssKgWHL._SS500_.jpg" 
-            affiliateLink="https://app.partnerboost.com/track/..." 
+            affiliateLink="https://app.partnerboost.com/track/df000UdWCUDYlnL78iUB1btaCk5rzIi_b8cLIu3p8BwaePm_bUTUWGEdWeGWfJtibX6V1VsV8_c?url=https%3A%2F%2Fwww.amazon.com%2Fdp%2FB08KHYP2KV%3Fmaas%3Dmaas_adg_api_581697951548837409_static_12_201%26ref_%3Daa_maas%26tag%3Dmaas%26aa_campaignid%3D640ae36f6729c4fc131912d9df106def%26aa_adgroupid%3Dfde6DiRIT2fXfUdGDRC7nXsS6yTYWUdxcWPBOWkp8wQyLESMpwErZFUs7OB0dsCbtWUggJ_bLi_aPbuC3Zngs_c%26aa_creativeid%3D5c5aLiI5_bXw53_bxXpBy2ADRqrpApbspiwuDVfjYaBGTMraE_c
+" 
           />
         </div>
       </section>
